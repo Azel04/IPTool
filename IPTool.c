56a303d30b212180125abfb8e0170ed61b2d4b54
Azel04 <75199595+Azel04@users.noreply.github.com>
2021-09-24 09:45:40 +0200

    Create IPTool.c

---
+++ IPTool.c
@@ -0,0 +1,56 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int ip_bin_dot[32];
+    int ip_dec_dot[4];
+    
+    int id[4] = {23, 15, 7, 0};
+    int temp;
+    
+    int i; 
+    int j;
+    int k;
+    
+    for(i = 0; i < 4; i++){
+        printf("Inserisci la cifra numero %d dell'indirizzo ipv4 in decimale: ", i + 1);
+        scanf("%d", &ip_dec_dot[i]);
+    }
+    
+    printf("L'indirizzo inserito e' il seguente: ");
+    for(i = 0; i < 4; i++){
+        if(i == 3){
+            printf("%d", ip_dec_dot[i]);
+        }
+        else{
+            printf("%d.", ip_dec_dot[i]);
+        }
+    }
+    i = 0;
+    j = 7;
+    k = 0;
+    do{
+        do{
+            ip_bin_dot[j] = ip_dec_dot[i] % 2;
+            ip_dec_dot[i] = ip_dec_dot[i] / 2;
+            j--;
+            k++;
+        }while(k < 8);
+        i++;
+        j = 8 * i + 7; // Per esempio: 8 - 1 + 8 = 15
+        k = 0;
+    }while(i < 4);
+    
+    printf("\nL'indirizzo ip in binario e' il seguente: \n");
+    j = 0;
+    k = 8;
+    for(i = 0; i < 32; i++){
+      if(i % 8 == 0 && i != 0){
+        printf(".");
+      }
+      printf("%d", ip_bin_dot[i]);
+    }
+
+    return 0;
+}
