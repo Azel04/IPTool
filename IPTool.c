1a2b02bfa1005e06d89514fa289ca39fe489aaa9
Azel04 <75199595+Azel04@users.noreply.github.com>
2021-09-24 12:22:59 +0200

    Update IPTool.c

--- IPTool.c
+++ IPTool.c
@@ -5,16 +5,15 @@
 #include "controllo_tipo_ip.h"
 
 int main(){
-    int ip_bin_dot[32];
-    int ip_dec_dot[4];
-    
-	input_ip(ip_dec_dot);
-    printf("L'indirizzo inserito e' il seguente: ");
-    output_ip_dec(ip_dec_dot);
-    printf("\n");
+	int ip_bin_dot[32];
+    	int ip_dec_dot[4];
+    	input_ip(ip_dec_dot);
+    	printf("L'indirizzo inserito e' il seguente: ");
+    	output_ip_dec(ip_dec_dot);
+    	printf("\n");
 	controllo_tipo_ip(ip_dec_dot);
 	conversione_decimale_binario(ip_dec_dot, ip_bin_dot);
-    printf("\nL'indirizzo ip in binario e' il seguente: \n");
+    	printf("\nL'indirizzo ip in binario e' il seguente: \n");
 	output_ip_bin(ip_bin_dot);
 
     return 0;
