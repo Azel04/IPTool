5da920b8fbe6eace77e7ea5492ad825b57543de9
Azel04 <75199595+Azel04@users.noreply.github.com>
2021-09-24 12:13:00 +0200

    Update IPTool.c

--- IPTool.c
+++ IPTool.c
@@ -1,81 +1,21 @@
+#include "stdio.h"
+#include "input_ip.h"
+#include "output_ip.h"
+#include "conversione_decimale_binario_ip.h"
+#include "controllo_tipo_ip.h"
 
-#include <stdio.h>
-
-int main()
-{
+int main(){
     int ip_bin_dot[32];
     int ip_dec_dot[4];
     
-    int id[4] = {23, 15, 7, 0};
-    int temp;
-    
-    int i; 
-    int j;
-    int k;
-    
-    for(i = 0; i < 4; i++){
-      do{
-          printf("Inserisci la cifra numero %d dell'indirizzo ipv4 in decimale: ", i + 1);
-          scanf("%d", &ip_dec_dot[i]);
-      }while(ip_dec_dot[i] < 0 || ip_dec_dot[i] > 255);
-    }
-    
+	input_ip(ip_dec_dot);
     printf("L'indirizzo inserito e' il seguente: ");
-    for(i = 0; i < 4; i++){
-        if(i == 3){
-            printf("%d", ip_dec_dot[i]);
-        }
-        else{
-            printf("%d.", ip_dec_dot[i]);
-        }
-    }
+    output_ip_dec(ip_dec_dot);
     printf("\n");
-
-    switch(ip_dec_dot[0]){
-      case 10:
-        printf("L'indirizzo inserito e' un indirizzo privato di classe A\n");
-        break;
-      case 172:
-        if(ip_dec_dot[1] >= 16 && ip_dec_dot[1] <= 31){
-          printf("L'indirizzo inserito e' un indirizzo privato di classe B\n");
-        }
-        else{
-          printf("L'indirizzo inserito e' un indirizzo pubblico di classe B\n");
-        }
-      case 192:
-        if(ip_dec_dot[1] == 168){
-          printf("L'indirizzo inserito e' un indirizzo privato di classe C\n");
-        }
-        else{
-          printf("L'indirizzo inserito e' un indirizzo pubblico di classe C\n");
-        }
-    }
-
-    //INIZIO CONVERSIONE DA DEC A BIN
-    i = 0;
-    j = 7;
-    k = 0;
-    do{
-        do{
-            ip_bin_dot[j] = ip_dec_dot[i] % 2;
-            ip_dec_dot[i] = ip_dec_dot[i] / 2;
-            j--;
-            k++;
-        }while(k < 8);
-        i++;
-        j = 8 * i + 7; // Per esempio: 8 - 1 + 8 = 15
-        k = 0;
-    }while(i < 4);
-    //INIZIO STAMPA IP BIN
+	controllo_tipo_ip(ip_dec_dot);
+	conversione_decimale_binario(ip_dec_dot, ip_bin_dot);
     printf("\nL'indirizzo ip in binario e' il seguente: \n");
-    j = 0;
-    k = 8;
-    for(i = 0; i < 32; i++){
-      if(i % 8 == 0 && i != 0){
-        printf(".");
-      }
-      printf("%d", ip_bin_dot[i]);
-    }
+	output_ip_bin(ip_bin_dot);
 
     return 0;
 }
