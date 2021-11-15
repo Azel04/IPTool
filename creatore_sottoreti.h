e2ecfd75d368484456136d87761964f1303c82fb
Azel04 <jacoporossettij.r.6@gmail.com>
2021-11-15 16:46:28 +0100

    Pulizia codice
    Ho sostituito le miriade di variabili con la dichiarazione di una struct con tutte le variabili utili.

--- creatore_sottoreti.h
+++ creatore_sottoreti.h
@@ -2,14 +2,21 @@ void creatore_sottoreti_c(int *ip_dec_dot);
 void creatore_sottoreti_b(int *ip_dec_dot);
 void creatore_sottoreti_a(int *ip_dec_dot);
 
-void creatore_sottoreti_a(int *ip_dec_dot){
+typedef struct{
   int ip_bin_dot[32];
-  int clone_ip_dec_dot[4];
   int numero_sottorete_bin[32];
-  int numero_sottorete_bin_bit;
+  int gateway;
+  int range[2];
+  int host_per_sottorete;
+  int broadcast;
+  int net_id;
+}indirizzi;
+
+void creatore_sottoreti_a(int *ip_dec_dot){
+  indirizzi ip;
+  int clone_ip_dec_dot[4];  
   int num_sottoreti;
   int pot2;
-  int net_id;
   int j;
   int i;
   int k;
@@ -17,13 +24,11 @@ void creatore_sottoreti_a(int *ip_dec_dot){
   int cont;
   int intervallo_pot;
   int intervallo;
-  int broadcast;
   int ip_temp[32];
-  int gateway;
-  int range[2];
+
   pot2 = 0;
   cont_pot = 0;
-  net_id = 0;
+  ip.net_id = 0;
   do{
     printf("Digita il numero di sottoreti da creare: ");
     scanf("%d", &num_sottoreti);
@@ -40,57 +45,59 @@ void creatore_sottoreti_a(int *ip_dec_dot){
     clone_ip_dec_dot[i] = ip_dec_dot[i];
   }
   printf("NETWORK ID\tGATEWAY\tBROADCAST\t\tPRIMO HOST\tULTIMO HOST\n");
-  conversione_decimale_binario(ip_dec_dot, ip_bin_dot);
+  conversione_decimale_binario(ip_dec_dot, ip.ip_bin_dot);
   for(cont = 0; cont < num_sottoreti; cont++){
     
     for(i = 0; i < 32; i++){
-      ip_temp[i] = ip_bin_dot[i];
+      ip_temp[i] = ip.ip_bin_dot[i];
     }
     for(i = 7 + cont_pot; i < 32; i++){
       ip_temp[i] = 0;
     }
-    conversione_binario_decimale(ip_dec_dot, ip_bin_dot);
+    conversione_binario_decimale(ip_dec_dot, ip.ip_bin_dot);
     printf("%d.%d.%d.%d\t", ip_dec_dot[0], ip_dec_dot[1], ip_dec_dot[2], ip_dec_dot[3]);
     for(i = 0; i < 32; i++){
-      ip_temp[i] = ip_bin_dot[i];
+      ip_temp[i] = ip.ip_bin_dot[i];
     }
     ip_temp[31] = 1;
     conversione_binario_decimale(ip_dec_dot, ip_temp);
-    range[0] = ip_dec_dot[3] + 1;
+    ip.range[0] = ip_dec_dot[3] + 1;
     printf("%d.%d.%d.%d\t", ip_dec_dot[0], ip_dec_dot[1], ip_dec_dot[2], ip_dec_dot[3]);
     for(i = 0; i < 32; i++){
-      ip_temp[i] = ip_bin_dot[i];
+      ip_temp[i] = ip.ip_bin_dot[i];
     }
     for(i = 7 + cont_pot; i < 32; i++){
       ip_temp[i] = 1;
     }  
     conversione_binario_decimale(ip_dec_dot, ip_temp);
     printf("%d.%d.%d.%d\t", ip_dec_dot[0], ip_dec_dot[1], ip_dec_dot[2], ip_dec_dot[3]);
-    range[1] = ip_dec_dot[3] - 1;
-    printf("%d.%d.%d.%d\t", ip_dec_dot[0], ip_dec_dot[1], ip_dec_dot[2], range[0]);
-    printf("%d.%d.%d.%d\t", ip_dec_dot[0], ip_dec_dot[1], ip_dec_dot[2], range[1]);
-    conversione_decimale_binario_semplice(cont + 1, numero_sottorete_bin);
+    ip.range[1] = ip_dec_dot[3] - 1;
+    printf("%d.%d.%d.%d\t", ip_dec_dot[0], ip_dec_dot[1], ip_dec_dot[2], ip.range[0]);
+    printf("%d.%d.%d.%d\t", ip_dec_dot[0], ip_dec_dot[1], ip_dec_dot[2], ip.range[1]);
+    conversione_decimale_binario_semplice(cont + 1, ip.numero_sottorete_bin);
     j = 0;
     k = 31;
     for(i = 7 + cont_pot - 1; j <= cont_pot - 1; j++){
-      ip_bin_dot[i] = numero_sottorete_bin[k];
+      ip.ip_bin_dot[i] = ip.numero_sottorete_bin[k];
       k--;
       i--;
     }
     printf("\n\n");
   }
-  printf("INDIRIZZO IP CON CIDR: %d.0.0.0/%d\n", clone_ip_dec_dot[0], 8 + (cont_pot - 1));
+  ip.host_per_sottorete = 0;
+    while(ip.range[0] != ip.range[1]){
+      ip.host_per_sottorete++;
+      ip.range[0]++;
+    }
+    printf("INDIRIZZO IP CON CIDR: %d.0.0.0/%d\nNumero Host per sottorete: %d\n", clone_ip_dec_dot[0], 8 + (cont_pot - 1), ip.host_per_sottorete);
 
 }
 
 void creatore_sottoreti_b(int *ip_dec_dot){
-  int ip_bin_dot[32];
+  indirizzi ip;
   int clone_ip_dec_dot[4];
-  int numero_sottorete_bin[32];
-  int numero_sottorete_bin_bit;
   int num_sottoreti;
   int pot2;
-  int net_id;
   int j;
   int i;
   int k;
@@ -98,13 +105,11 @@ void creatore_sottoreti_b(int *ip_dec_dot){
   int cont;
   int intervallo_pot;
   int intervallo;
-  int broadcast;
   int ip_temp[32];
-  int gateway;
-  int range[2];
+
   pot2 = 0;
   cont_pot = 0;
-  net_id = 0;
+  ip.net_id = 0;
   do{
     printf("Digita il numero di sottoreti da creare: ");
     scanf("%d", &num_sottoreti);
@@ -119,117 +124,126 @@ void creatore_sottoreti_b(int *ip_dec_dot){
   intervallo = 256 / pot2;
   
   printf("NETWORK ID\tGATEWAY\tBROADCAST\t\tPRIMO HOST\tULTIMO HOST\n");
-  conversione_decimale_binario(ip_dec_dot, ip_bin_dot);
+  conversione_decimale_binario(ip_dec_dot, ip.ip_bin_dot);
   for(cont = 0; cont < num_sottoreti; cont++){
 
     for(i = 0; i < 32; i++){
-      ip_temp[i] = ip_bin_dot[i];
+      ip_temp[i] = ip.ip_bin_dot[i];
     }
     for(i = 15 + cont_pot; i < 32; i++){
       ip_temp[i] = 0;
     }
-    conversione_binario_decimale(ip_dec_dot, ip_bin_dot);
+    conversione_binario_decimale(ip_dec_dot, ip.ip_bin_dot);
     printf("%d.%d.%d.%d\t", ip_dec_dot[0], ip_dec_dot[1], ip_dec_dot[2], ip_dec_dot[3]);
     for(i = 0; i < 32; i++){
-      ip_temp[i] = ip_bin_dot[i];
+      ip_temp[i] = ip.ip_bin_dot[i];
     }
     ip_temp[31] = 1;
     conversione_binario_decimale(ip_dec_dot, ip_temp);
-    range[0] = ip_dec_dot[3] + 1;
+    ip.range[0] = ip_dec_dot[3] + 1;
     printf("%d.%d.%d.%d\t", ip_dec_dot[0], ip_dec_dot[1], ip_dec_dot[2], ip_dec_dot[3]);
     for(i = 0; i < 32; i++){
-      ip_temp[i] = ip_bin_dot[i];
+      ip_temp[i] = ip.ip_bin_dot[i];
     }
     for(i = 15 + cont_pot; i < 32; i++){
       ip_temp[i] = 1;
     }  
     conversione_binario_decimale(ip_dec_dot, ip_temp);
     printf("%d.%d.%d.%d\t", ip_dec_dot[0], ip_dec_dot[1], ip_dec_dot[2], ip_dec_dot[3]);
-    range[1] = ip_dec_dot[3] - 1;
-    printf("%d.%d.%d.%d\t", ip_dec_dot[0], ip_dec_dot[1], ip_dec_dot[2], range[0]);
-    printf("%d.%d.%d.%d\t", ip_dec_dot[0], ip_dec_dot[1], ip_dec_dot[2], range[1]);
-    conversione_decimale_binario_semplice(cont + 1, numero_sottorete_bin);
+    ip.range[1] = ip_dec_dot[3] - 1;
+    printf("%d.%d.%d.%d\t", ip_dec_dot[0], ip_dec_dot[1], ip_dec_dot[2], ip.range[0]);
+    printf("%d.%d.%d.%d\t", ip_dec_dot[0], ip_dec_dot[1], ip_dec_dot[2], ip.range[1]);
+    conversione_decimale_binario_semplice(cont + 1, ip.numero_sottorete_bin);
     j = 0;
     k = 31;
     for(i = 15 + cont_pot - 1; j <= cont_pot - 1; j++){
-      ip_bin_dot[i] = numero_sottorete_bin[k];
+      ip.ip_bin_dot[i] = ip.numero_sottorete_bin[k];
       k--;
       i--;
     }
     printf("\n\n");
   }
-  printf("INDIRIZZO IP CON CIDR: %d.%d.0.0/%d\n", clone_ip_dec_dot[0], clone_ip_dec_dot[1], 16 + (cont_pot - 1));
+    ip.host_per_sottorete = 0;
+    while(ip.range[0] != ip.range[1]){
+      ip.host_per_sottorete++;
+      ip.range[0]++;
+    }
+    printf("INDIRIZZO IP CON CIDR: %d.0.0.0/%d\nNumero Host per sottorete: %d\n", clone_ip_dec_dot[0], 8 + (cont_pot - 1), ip.host_per_sottorete);
 }
 
 
 void creatore_sottoreti_c(int *ip_dec_dot){
-    int pot2;
-    int num_sottoreti;
-    int net_id;
-    int broadcast;
-    int gateway;
-    int range[2];
-    int ip_bin_dot[32];
-    int clone_ip_dec_dot[4];
-    int cont_pot;
-    int cont;
-    int cont2;
-    int intervallo;
-    int intervallo_pot;
+  indirizzi ip;
+  int clone_ip_dec_dot[4];
+  int num_sottoreti;
+  int pot2;
+  int j;
+  int i;
+  int cont2;
+  int k;
+  int cont_pot;
+  int cont;
+  int intervallo_pot;
+  int intervallo;
+  int ip_temp[32];
+  pot2 = 0;
+  num_sottoreti =0 ;
+  ip.net_id = 0;
+  ip.broadcast = 0;
+  ip.gateway = 0;
+  ip.range[0] = 0;
+  ip.range[1] = 0;
+  cont_pot = 0;
+  cont = 0;
+  intervallo = 0;
+  ip_dec_dot[3] = 0;
 
-    pot2 = 0;
-    num_sottoreti =0 ;
-    net_id = 0;
-    broadcast = 0;
-    gateway = 0;
-    range[0] = 0;
-    range[1] = 0;
-    cont_pot = 0;
-    cont = 0;
-    intervallo = 0;
-    ip_dec_dot[3] = 0;
+  for(cont = 0; cont < 4; cont++){
+    clone_ip_dec_dot[cont] = ip_dec_dot[cont];
+  }
 
-    for(cont = 0; cont < 4; cont++){
-        clone_ip_dec_dot[cont] = ip_dec_dot[cont];
-    }
-
-    do{
-        printf("Digita il numero di sottoreti da creare: ");
-        scanf("%d", &num_sottoreti);
-		if(num_sottoreti <= 0 || num_sottoreti >= 64){
+  do{
+    printf("Digita il numero di sottoreti da creare: ");
+    scanf("%d", &num_sottoreti);
+	  if(num_sottoreti <= 0 || num_sottoreti >= 64){
 			printf("Non e' possibile soddisfare la tua richiesta.\n");
-		}
-    }while(num_sottoreti <= 0 || num_sottoreti >= 64);
-    do{
-        pot2 = pow(2,cont_pot);
-        cont_pot++;
-    }while(pot2 <= num_sottoreti);
-    intervallo = 256 / pot2;
+	  }
+  }while(num_sottoreti <= 0 || num_sottoreti >= 64);
+  do{
+    pot2 = pow(2,cont_pot);
+    cont_pot++;
+  }while(pot2 <= num_sottoreti);
+  intervallo = 256 / pot2;
 
-    net_id = 0;
+  ip.net_id = 0;
     
-    printf("\tNETWORK ID\tBROADCAST\tGATEWAY\t\tPRIMO HOST\tULTIMO HOST\n");
-    for(cont = 0; cont < num_sottoreti; cont++){
-        broadcast = net_id + intervallo - 1;
-        gateway = net_id + 1;
-        range[0] = gateway + 1;
-        range[1] = broadcast - 1;
-		    printf("%d)\t%d.%d.%d.%03d", cont + 1, ip_dec_dot[0], ip_dec_dot[1], ip_dec_dot[2], ip_dec_dot[3]);
-		    printf("\t%d.%d.%d.%03d", ip_dec_dot[0], ip_dec_dot[1], ip_dec_dot[2], broadcast);
-		    printf("\t%d.%d.%d.%03d", ip_dec_dot[0], ip_dec_dot[1], ip_dec_dot[2], gateway);
-		    printf("\t%d.%d.%d.%03d", ip_dec_dot[0], ip_dec_dot[1], ip_dec_dot[2], range[0]);
-		    printf("\t%d.%d.%d.%03d\n", ip_dec_dot[0], ip_dec_dot[1], ip_dec_dot[2], range[1]);		
-        printf("\t");
-        conversione_decimale_binario(ip_dec_dot, ip_bin_dot);
-        output_ip_bin_sottorete_c(ip_bin_dot, (8 - intervallo_pot));
-        net_id = net_id + intervallo;
-        ip_dec_dot[3] = net_id;
-        for(cont2 = 0; cont2 < 3; cont2++){
-            ip_dec_dot[cont2] = clone_ip_dec_dot[cont2];
-        }
-
-        printf("\n\n");
+  printf("\tNETWORK ID\tBROADCAST\tGATEWAY\t\tPRIMO HOST\tULTIMO HOST\n");
+  for(cont = 0; cont < num_sottoreti; cont++){
+    ip.broadcast = ip.net_id + intervallo - 1;
+    ip.gateway = ip.net_id + 1;
+    ip.range[0] = ip.gateway + 1;
+    ip.range[1] = ip.broadcast - 1;
+		printf("%d)\t%d.%d.%d.%03d", cont + 1, ip_dec_dot[0], ip_dec_dot[1], ip_dec_dot[2], ip_dec_dot[3]);
+		printf("\t%d.%d.%d.%03d", ip_dec_dot[0], ip_dec_dot[1], ip_dec_dot[2], ip.broadcast);
+		printf("\t%d.%d.%d.%03d", ip_dec_dot[0], ip_dec_dot[1], ip_dec_dot[2], ip.gateway);
+		printf("\t%d.%d.%d.%03d", ip_dec_dot[0], ip_dec_dot[1], ip_dec_dot[2], ip.range[0]);
+		printf("\t%d.%d.%d.%03d\n", ip_dec_dot[0], ip_dec_dot[1], ip_dec_dot[2], ip.range[1]);		
+    printf("\t");
+    conversione_decimale_binario(ip_dec_dot, ip.ip_bin_dot);
+    output_ip_bin_sottorete_c(ip.ip_bin_dot, (8 - intervallo_pot));
+    ip.net_id = ip.net_id + intervallo;
+    ip_dec_dot[3] = ip.net_id;
+    for(cont2 = 0; cont2 < 3; cont2++){
+      ip_dec_dot[cont2] = clone_ip_dec_dot[cont2];
     }
-    printf("INDIRIZZO IP CON CIDR: %d.%d.%d.0/%d\n", ip_dec_dot[0], ip_dec_dot[1], ip_dec_dot[2], 24 + (cont_pot - 1));
+
+    printf("\n\n");
+    }
+    ip.host_per_sottorete = 0;
+    while(ip.range[0] != ip.range[1]){
+      ip.host_per_sottorete++;
+      ip.range[0]++;
+    }
+    printf("INDIRIZZO IP CON CIDR: %d.0.0.0/%d\nNumero Host per sottorete: %d\n", clone_ip_dec_dot[0], 8 + (cont_pot - 1), ip.host_per_sottorete);
 
 }
