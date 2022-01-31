23a13283e7146da33b4519d97dfa9977aa7d22f4
Azel04 <75199595+Azel04@users.noreply.github.com>
2022-01-31 18:55:00 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -34,7 +34,7 @@ int main(){
 	sleep(2);
 	clearscreen(); //Pulizia schermo
 	do{
-		printf("1) Inserisci un indirizzo ipv4 e calcolane la sua classe e il suo corrispettivo ip in binario\n2) Crea un numero di sottoreti a tua scelta con la FLSM\n3) Crea un numero di sottoreti a tua scelta con la VLSM\n4) Crea un numero di sottoreti a tua scelta con il supernetting in FLSM\n5) Verifica se 2 indirizzi appartengono alla stessa subnet\n6) Fine\nFai la tua scelta: ");
+		printf("1) Inserisci un indirizzo ipv4 e calcolane la sua classe e il suo corrispettivo ip in binario\n2) Crea un numero di sottoreti a tua scelta con la FLSM\n3) Crea un numero di sottoreti a tua scelta con la VLSM\n4) Crea un numero di sottoreti a tua scelta con il supernetting (ATTENZIONE: LA FUNZIONE NON È COMPLETA, L'UTILIZZO È SCONSIGLIATO)\n5) Verifica se 2 indirizzi appartengono alla stessa subnet\n6) Fine\nFai la tua scelta: ");
 		scanf("%d", &scelta);
 		switch(scelta){
 			case 1:
